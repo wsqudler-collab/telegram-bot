501d86a74fbab4d146810660a8b002c831e39a7a
wsqudler-collab <wsqudler@gmail.com>
2026-05-11 18:47:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает!")
 
 def main():
+    if not TOKEN:
+        print("NO TOKEN FOUND")
+        return
+
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
