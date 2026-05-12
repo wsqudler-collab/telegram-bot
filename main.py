46f0512549341edd42535510c50a7d9b14123c28
wsqudler-collab <wsqudler@gmail.com>
2026-05-12 13:18:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,16 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("8733489007:AAFYFiMbER9Cp00XTid5TjB3az6AftPpMNo")
+TOKEN = os.getenv("8733489007:AAH_So9kjUljYsbgXDjcwty81IcoebKelwg")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Я работаю на Railway")
+    await update.message.reply_text("Бот работает")
 
 def main():
     if not TOKEN:
-        raise RuntimeError("TELEGRAM_TOKEN не найден в переменных окружения Railway")
-
+        raise RuntimeError("TELEGRAM_TOKEN не найден")
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-
-    # Если раньше пробовала webhook, удаляем его и работаем через polling
     app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
